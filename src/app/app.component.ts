6892593405583270610161f41954cf4e1a2a08e1
SaraAmari <sara.amari@esprit.tn>
2019-09-15 00:08:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+//  constructor (private httpService: HttpClient) { }
+ // arrBirds: string [];
+
+  ngOnInit () {
+  /*  this.httpService.get('./assets/data.json').subscribe(
+      data => {
+        this.arrBirds = data as string [];	
+        console.log("ahawmaaa " +data);
+        // FILL THE ARRAY WITH DATA.
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+
+
+
+      
+    );
+*/
+  }
 }
